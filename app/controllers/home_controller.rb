41b605ddb6d60183aac90587847806d5d764ffeb
Corey Gibson <cgibsonmm@gmail.com>
2017-07-27 18:05:36 -0400

    added basic user list

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    session[:conversations] ||= []
+
+    @user = User.all.where.not(id: current_user)
+    @conversations = Conversation.includes(:recipient, :messages)
+                                 .find(session[:conversations])
   end
 end
